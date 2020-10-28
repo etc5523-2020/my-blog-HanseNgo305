a0513ee45a496f1dbc08ed1b53cafeae1d068c89
“HanseNgo305” <hngo0005@student.monash.edu>
2020-10-28 23:40:57 +0700

    Add the overview

---
+++ r-package-assessment-dedi0003-master/covidasean/R/covid_asean_df.R
@@ -0,0 +1,22 @@
+#' Covid ASEAN Dataset
+#'
+#' This is dataset used for producing covid ASEAN shiny dashboard. The dataset contains covid-19 cases data from Indonesia, Malaysia, Philippines, Singapore and Thailand.
+#'
+#'
+#'
+#' @format a tibble with 203 rows and 53 columns.
+#'
+#' @examples
+#' head(covid_asean_df)
+#'
+#'
+#' @references Gassen, Joachim. 2020. Tidycovid19: Download, Tidy and Visualize Covid-19 Related Data.
+#' (\href{https://joachim-gassen.github.io/tidycovid19/}{Tidycovid19})
+#'
+#'
+#' @docType data
+#'
+#'
+"covid_asean_df"
+
+
